4ee58222dcac80e6112f151dbc9f687666656e36
cosmincaloian <cos2006c@gmail.com>
2023-06-20 17:08:52 +0300

    First Commit

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Poll from './components/Poll.vue'
+</script>
+
+<template>
+  <main>
+    <Poll />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ client/src/components/Poll.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import PollItem from './PollItem.vue'
+import * as process from "process";
+
+function _refresh() {
+// const response = await fetch(process.env.SERVER_URL + '/poll');
+// const data = await response.json();
+
+}
+
+const data = [
+  {
+    "label": "first",
+    "percentage": "10%"
+  },
+  {
+    "label": "second",
+    "percentage": "20%"
+  }
+]
+</script>
+
+<template>
+  <div class="poll">
+    <div class="poll-title">Which one do you prefer?</div>
+    <PollItem v-for="{ label, percentage } in data">
+      <template #info>
+        <div class="poll-item-label">{{ label }}</div>
+        <div class="poll-item-percentage">{{ percentage }}</div>
+      </template>
+    </PollItem>
+  </div>
+</template>
+
