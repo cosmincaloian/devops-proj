301a85fd77b3fdbc6fe6c702dfb0c43b46189756
cosmincaloian <cos2006c@gmail.com>
2023-06-21 11:53:57 +0300

    update

--- client/src/components/DevopsPoll.vue
+++ client/src/components/DevopsPoll.vue
@@ -19,11 +19,11 @@ function add() {
 <template>
   <div class="poll">
     <div class="poll-title">Which one do you prefer?</div>
-    <div class="poll-item" @click="add" v-for="{ label, percentage } in data">
+    <div class="poll-item" @click="add" v-for="(item, index) in data" :key="item.label">
       <div class="poll-item-fill"></div>
       <div class="poll-item-info">
-        <div class="poll-item-label">{{ label }}</div>
-        <div class="poll-item-percentage">{{ percentage }}</div>
+        <div class="poll-item-label">{{ item.label }}</div>
+        <div class="poll-item-percentage">{{ item.percentage }}</div>
       </div>
     </div>
   </div>
